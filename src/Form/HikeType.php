5f98997050d2c6358fe9ef688ed6fc109340e7b0
Jessica Wastyn <[jessica.wastyn@outlook.fr] (mailto:jessica.wastyn@outlook.fr)>
2024-05-23 16:03:46 +0200

    Création CRUD Admin

---
+++ src/Form/HikeType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Hike;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HikeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('idHike')
+            ->add('nameHike')
+            ->add('descHike')
+            ->add('cityHike')
+            ->add('timeHike')
+            ->add('distHike')
+            ->add('heightHike')
+            ->add('downHike')
+            ->add('difficultyHike')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Hike::class,
+        ]);
+    }
+}
